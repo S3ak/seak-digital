f5dacdfd2356360649c7dd74ea0651adec200547
S3ak <nsineke@gmail.com>
2020-05-01 17:35:25 +0200

    Add styled tools and knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../stories/**/*.stories.js", "../components/**/*/*.stories.js"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs/register",
+  ],
 };
---
+++ stories/palette.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default {
+  title: "Palette",
+};
+
+export const ToStorybook = () => <div></div>;
+
+ToStorybook.story = {
+  name: "palette",
+};
