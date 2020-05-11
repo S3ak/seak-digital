3881cfb31cdbcbdf8ad71d30c50fc58654d3fe00
S3ak <nsineke@gmail.com>
2020-05-11 16:53:53 +0200

    Some more styling

--- components/user-inputs/buttons/cta-round/index.js
+++ components/user-inputs/buttons/cta-round/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Wrapper, InnerCircle } from "./styled";
 
-export const CTARound = ({ children, theme, mode, priority }) => {
+export const CTARound = ({ children, theme, mode, priority, onClick }) => {
   return (
-    <Wrapper theme={theme} mode={mode} priority={priority}>
+    <Wrapper theme={theme} mode={mode} priority={priority} onClick={onClick}>
       <InnerCircle theme={theme} mode={mode}>
         {children}
       </InnerCircle>
--- components/user-inputs/link/styled.js
+++ components/user-inputs/link/styled.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const Wrapper = styled.a`
   color: var(--color-primary);
+  text-decoration: none;
 `;
