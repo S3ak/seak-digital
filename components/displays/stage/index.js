2622beae3737469c5c395939d42c7dca31403b67
S3ak <nsineke@gmail.com>
2020-05-11 15:58:22 +0200

    Updatestage component

--- components/displays/stage/index.js
+++ components/displays/stage/index.js
@@ -2,8 +2,16 @@ import React from "react";
 
 import { Wrapper } from "./styled";
 
-export const stage = ({ children }) => {
-  return <Wrapper className="c-stage_main">{children}</Wrapper>;
+export const stage = ({ children, hasTopOffset, isInverse }) => {
+  return (
+    <Wrapper
+      className="c-stage_main"
+      hasTopOffset={hasTopOffset}
+      isInverse={isInverse}
+    >
+      {children}
+    </Wrapper>
+  );
 };
 
 export default stage;
