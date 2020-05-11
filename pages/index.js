82e328cafc4e4d0ef49bc47784ea54904baad04e
S3ak <nsineke@gmail.com>
2020-05-11 15:58:54 +0200

    Update page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
+import { FaAngleDown, FaGithub } from "react-icons/fa";
 
-import BtnSubmit from "../components/user-inputs/buttons/submit";
+// import BtnSubmit from "../components/user-inputs/buttons/submit";
 import BtnCTARound from "../components/user-inputs/buttons/cta-round";
-import BtnCTALandscape from "../components/user-inputs/buttons/cta-landscape";
+import LinkButton from "../components/user-inputs/buttons/link";
+import Link from "../components/user-inputs/link";
+// import BtnCTALandscape from "../components/user-inputs/buttons/cta-landscape";
 import Gallery from "../components/user-inputs/gallery";
 import IntroArticle from "../components/displays/intro-article";
-import InputField from "../components/user-inputs/input-fields/base";
+// import InputField from "../components/user-inputs/input-fields/base";
 import Header from "../components/displays/header";
 import Stage from "../components/displays/stage";
 import Footer from "../components/displays/footer";
@@ -58,48 +61,54 @@ export default function Home() {
                     web...
                   </p>
 
-                  <BtnCTARound kind="text" mode="primary" icon="angle-down">
+                  <LinkButton kind="text" theme="primary" icon="angle-down">
                     Read More
-                  </BtnCTARound>
+                  </LinkButton>
                 </IntroArticle>
               </IntroSection>
 
               <div className="l-page_divider">
                 <div className="c-button-group">
-                  <BtnCTARound kind="icon" mode="inverse" />
-                  <BtnCTARound kind="icon" mode="secondary" />
+                  <div className="c-button-group_item">
+                    <BtnCTARound kind="icon" theme="inverse" priority="primary">
+                      <FaAngleDown />
+                    </BtnCTARound>
+                  </div>
+
+                  <div className="c-button-group_item">
+                    <BtnCTARound
+                      kind="icon"
+                      theme="secondary"
+                      icon="angle-down"
+                      priority="secondary"
+                    >
+                      <FaGithub />
+                    </BtnCTARound>
+                  </div>
                 </div>
               </div>
             </article>
           </Container>
         </Stage>
 
-        <Stage>
+        <Stage hasTopOffset isInverse>
           <Container>
             <article className="c-stage_header">
-              <div className="c-section-title">
-                <h2 className="c-section-title_heading">Contact</h2>
-                <p className="c-section-title_subtitle">Let's chat</p>
-                <p className="c-section-title_description">
-                  <a href="mailto:nsineke@gmail.com">nsineke@gmail.com</a>
+              <div className="c-section-heading">
+                <div className="c-section-heading_first-row">
+                  <h2 className="c-section-heading_title">
+                    Con
+                    <br />
+                    tact
+                  </h2>
+
+                  <p className="c-section-heading_subtitle">Let's chat</p>
+                </div>
+
+                <p className="c-section-heading_description">
+                  <Link href="mailto:nsineke@gmail.com">nsineke@gmail.com</Link>
                 </p>
               </div>
-            </article>
-
-            <article className="c-stage_main">
-              <form>
-                <InputField name="e-mail" type="email" />
-                <BtnCTALandscape
-                  kind="landscape"
-                  mode="inverse"
-                  priority="primary"
-                  icon="mic"
-                >
-                  Leave me a note
-                </BtnCTALandscape>
-                <InputField name="note" type="text" />
-                <BtnSubmit icon="mail">Send</BtnSubmit>
-              </form>
             </article>
           </Container>
         </Stage>
@@ -111,9 +120,79 @@ export default function Home() {
 
       <style jsx>{`
         .l-page {
+          width: 100%;
           height: 100vh;
+          display: grid;
+          grid-template-columns: auto;
+          grid-template-rows: 0 1fr auto;
+          align-content: stretch;
+          grid-template-areas:
+            "header"
+            "main"
+            "footer";
+        }
+
+        .l-page_header {
+          grid-area: header;
+          height: 0;
+        }
+
+        .l-page_main {
+          grid-area: main;
+          overflow: auto;
           display: flex;
           flex-direction: column;
+        }
+
+        .l-page_footer {
+          grid-area: footer;
+        }
+
+        .l-page_divider {
+          margin-bottom: -23px;
+        }
+
+        .c-button-group {
+          display: flex;
+          justify-content: flex-end;
+        }
+
+        .c-button-group_item:first-child {
+          padding-right: 20px;
+        }
+
+        .c-section-title {
+        }
+
+        .c-section-heading_first-row {
+          display: flex;
+          align-items: flex-end;
+          flex-wrap: wrap;
+        }
+
+        .c-section-heading_title {
+          font-family: Roboto;
+          font-style: normal;
+          font-weight: 900;
+          font-size: 91px;
+          line-height: 73.19%;
+          /* or 67px */
+
+          font-variant: small-caps;
+          font-feature-settings: "cpsp" on;
+
+          /* Seak palette - Accent */
+
+          color: #ad9643;
+          /* Elevation / Dialog */
+
+          text-shadow: 0px 11px 15px rgba(0, 0, 0, 0.2),
+            0px 24px 38px rgba(0, 0, 0, 0.14);
+        }
+
+        .c-section-heading_description {
+          padding-top: 20px;
+          color: var(--color-primary);
         }
 
         @media (max-width: 600px) {
@@ -137,6 +216,9 @@ export default function Home() {
           --color-intent-text: #fff;
 
           --size-global-horizontal-bleed: 20px;
+
+          --color-primary: #ee4b0e;
+          --color-muted: #bdbdbd;
         }
 
         html {
@@ -158,13 +240,22 @@ export default function Home() {
             sans-serif;
         }
 
+        footer {
+          grid
+        }
+
         h1,
         h2,
         h3,
         h4,
         h5,
-        h6 {
+        h6,
+        p {
           margin: 0;
+        }
+
+        a {
+          color: var(--color-primary);
         }
 
         * {
