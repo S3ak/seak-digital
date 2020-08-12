f627ff5da96f13b9aca2412b348bc37d1688bb0b
S3ak <nsineke@gmail.com>
2020-08-12 16:56:54 +0200

    Configuring lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,11 @@ module.exports = {
   ci: {
     upload: {
       target: ".build",
+      upload: {
+        target: "lhci",
+        serverBaseUrl: "https://your-lhci-server-url.example.com",
+        token: process.env.LHCI_GITHUB_APP_TOKEN, // could also use LHCI_TOKEN variable instead
+      },
     },
     collect: {
       staticDistDir: "build",
