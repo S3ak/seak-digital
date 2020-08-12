00a1e56e97ab10b3e54c6cd42b3e52d56fac05f9
S3ak <nsineke@gmail.com>
2020-08-13 00:21:25 +0200

    Fix storybook preview implementation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
 import WithGlobalStyles from "../utils/storybook-decorators/with-global-styles";
 
-addDecorator((story) => (
-  <div>
-    <WithGlobalStyles />
-    <story />
-  </div>
-));
+export const decorators = [
+  (Story) => (
+    <div>
+      <WithGlobalStyles />
+      <Story />
+    </div>
+  ),
+];
